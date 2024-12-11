40d8d24c71268a949b7a33f125ee121247a210cb
Jeff <jeffjunio.bt@gmail.com>
2024-12-10 21:34:28 -0300

    Feat: add exception global

--- src/main/java/dev/jeff/jbank/exception/WalletDataAlreadyExistsException.java
+++ src/main/java/dev/jeff/jbank/exception/WalletDataAlreadyExistsException.java
@@ -1,8 +1,23 @@
 package dev.jeff.jbank.exception;
 
+import org.springframework.http.ProblemDetail;
+
 public class WalletDataAlreadyExistsException extends JBankException {
 
-    public WalletDataAlreadyExistsException(String message) {
-        super(message);
+    private String detail;
+
+    public WalletDataAlreadyExistsException(String detail) {
+        super(detail);
+        this.detail = detail;
+    }
+
+    @Override
+    public ProblemDetail toProblemDetail() {
+        var pd = ProblemDetail.forStatus(422);
+
+        pd.setTitle("JBank Internal Server Error");
+        pd.setDetail(detail);
+
+        return pd;
     }
 }
